eb33a984fd974c783021990f484236a0cc16964f
Niral Pokal <niralpokal@gmail.com>
2016-03-01 14:18:34 -0800

    I created the Bootstrap template for where my reviews will go in my restuarants. #2

--- ProjectOne/default.js
+++ ProjectOne/default.js
@@ -4,6 +4,8 @@ var restuarant1 = {
   type: 'Mexican',
   numberOfReviews: 34,
   stars: 4.5
+  //image:
+  //reviews:
 };
 
 var restuarant2 = {
@@ -11,6 +13,8 @@ var restuarant2 = {
   type: 'Pizza',
   numberOfReviews: 14,
   stars: 3
+  //image:
+//  reviews:
 };
 
 var restuarant3 = {
@@ -18,6 +22,8 @@ var restuarant3 = {
   type: 'American',
   numberOfReviews: 198,
   stars: 4
+//  image:
+  //reviews:
 };
 
 var restuarant4 = {
@@ -25,6 +31,8 @@ var restuarant4 = {
   type: 'American',
   numberOfReviews: 56,
   stars: 3.5
+  //image:
+  //reviews:
 };
 
 var restuarant5 = {
@@ -32,6 +40,8 @@ var restuarant5 = {
   type: 'Pizza',
   numberOfReviews: 74,
   stars: 4
+  //image:
+  //reviews:
 };
 
 var restuarant6 = {
@@ -39,16 +49,128 @@ var restuarant6 = {
   type: 'American',
   numberOfReviews: 201,
   stars: 5
+  //image:
+  //reviews:
 };
 
 var restuarant7 = {
   name: 'Mod Pizza',
   type: 'Pizza',
   numberOfReviews: 93,
-  stars: 4.5
+  stars: 4.5,
+  //image:
+  //reviews:
 };
 
-var searchFail = 'Sorry but your search brought zero results.';
+var reviewsOfRestuarant1 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var reviewsOfRestuarant2 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var reviewsOfRestuarant3 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var reviewsOfRestuarant4 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var reviewsOfRestuarant5 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var reviewsOfRestuarant6 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var reviewsOfRestuarant7 =[
+  review1 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review2 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  },
+  review3 = {
+    name: 'Tom',
+    content: 'Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.'
+  }
+]
+
+var userReview = [];
+var searchFail = document.createTextNode('Sorry but your search brought zero results.');
+var failH1 = document.createElement('h1');
+failH1.appendChild(searchFail);
 var restuarantArray = [ restuarant1, restuarant2, restuarant3, restuarant4, restuarant5, restuarant6, restuarant7 ];
 var search;
 var restuarants = document.getElementById('restuarants');
@@ -56,14 +178,17 @@ var searchButton = document.getElementById('form1');
 
 
 
+
 function sortRestuarants(array,b) {
   for (var i = 0; i< array.length; i++){
     var a = array[i];
-    if ((a.type.toLowerCase() === b)|| (a.name.toLowerCase() === b)){
+    if ((a.type.toLowerCase() == b) || (a.name.toLowerCase() == b)){
       addRestuarant(array[i]);
+    } else /*if (!(a.type.toLowerCase() == b) && !(a.name.toLowerCase() == b))*/{
+      //restuarants.appendChild(failH1);
+      }
     }
-  }
-};
+  };
 
 function stopRefresh(event) {
   var element = restuarants;
