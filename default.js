9cd531e016975dc53f2a5f5915baf893695dbeeb
Niral Pokal <niralpokal@gmail.com>
2016-03-07 15:17:59 -0800

    I fixed a bug in the user review tags that created an infinite loop when the user clicked on one of the tags. #5

--- default.js
+++ default.js
@@ -7,7 +7,7 @@ var restaurant1 = {
   image: "images/mexican1.jpg",
   info: "Find us to try our savory tacos, our enormous burritos, and delightful antojitos. Don't forget to grab your freshly made agua fresca and to grab salsas from our 20+ salsa bar.",
   reviews: [
-    review1 = {
+    {
       name: 'Shawna C.',
       stars: 4,
       image: "images/mexicanreview1.jpg",
@@ -16,7 +16,7 @@ var restaurant1 = {
       cool: 0,
       content: "'Holy moly! Is this place GOOD! Don't let the look of the truck deceive you. At least for me, I had seen this truck numerous times at the food truck event that we frequent, but nothing really jumped out to me from the outside. The truck is a pretty plain black with simple gold small writing of Classic Tacos. Plus, you can find tacos at a lot of places. One day we finally decided to give it a shot and I really regret not trying these guys out sooner!!'"
     },
-    review2 = {
+    {
       name: 'Brian R.',
       stars: 5,
       image: "images/mexicanreview2.jpg",
@@ -25,7 +25,7 @@ var restaurant1 = {
       cool: 0,
       content: 'Man I read some of these reviews and damn some people really take their yelping very seriously. Straight and to the point. Food was great. Food was fast. The biggest challenge is my 3.5 year old daughter who is absurdly picky and she ate most of my burrito. Would eat again'
     },
-    review3 = {
+    {
       name: 'Angie M.',
       stars: 5,
       image: "images/mexicanreview3.jpg",
@@ -45,7 +45,7 @@ var restaurant2 = {
   image: "images/pizza1.jpg",
   info: 'We offer a flexible approach to dining, combining a fast paced, informal atmosphere in the daytime and casual sit down dinner service in the evening. Our thin, crisp pizzas combine a delicate balance of fresh, unique flavors paired with our carefully cured and handcrafted dough.',
   reviews: [
-    review1 ={
+    {
       name: 'Beast A.',
       stars: 2,
       image: "images/pizzareview1.jpg",
@@ -54,7 +54,7 @@ var restaurant2 = {
       cool: 0,
       content: "Ownership is rude. Staff is nice and on a weeknight the place seems like a good option but in reality it takes a long time to wait for food (20 minutes tonight) and there are better options really close by. I went to NY pizza cuz I'd been there in the past and didn't want to risk the new Square One or the CPK across Jeffrey since both are new and I wasn't sure how long the wait would be. But I certainly will be giving those a try next time! Can't be difficult to beat $26 medium pepperoni pizza with breadsticks and super easy to do better than the impolite owner with tons of attitude. Im all for buying local but why patronize a small business if they are going to be rude?"
     },
-    review2 = {
+    {
       name: 'Dana K.',
       stars: 4,
       image: "images/pizzareview2.jpg",
@@ -63,7 +63,7 @@ var restaurant2 = {
       cool: 0,
       content: 'I love pizza by the slice since I can get a variety. My default is the white pizza with mozzarella and ricotta and sprinkles of pepper flakes. Their garlic knots are super garlicy and bready which I absolutely love. They use good sausage (nice and peppery) on their pepperoni and sausage pizza.'
     },
-    review3 = {
+    {
       name: 'Aaron E.',
       stars: 3,
       image: "images/pizzareview3.jpg",
@@ -83,7 +83,7 @@ var restaurant3 = {
   image: 'images/american1.jpg',
   info: "We're farm to plate and won't break the bank! It's true. Farm means freshly delivered from local farms and producers. Plate means chef-crafted dishes piled high with one-of-a-kind flavors. Won't break the bank means you could pay twice as much for food this good.",
   reviews: [
-    review1 = {
+    {
       name: 'Julie H.',
       stars: 4,
       image: "images/americanreview1.jpg",
@@ -92,7 +92,7 @@ var restaurant3 = {
       cool: 0,
       content: 'My first impression of Urban Plates was that it was like a Lemonade done better. When I first walked in I immediately liked the variety in the seating arrangement. It includes a long narrow table on which people propped up their laptops and were working while others were able to eat and chat with the person across from them. They also offered 2 and 4 person tables around this longer table and plenty of outdoor seating on their covered patio.'
     },
-    review2 = {
+    {
       name: 'Anh B.',
       stars: 2,
       image: "images/americanreview2.jpg",
@@ -101,7 +101,7 @@ var restaurant3 = {
       cool: 0,
       content: "The dinner team tonight executed service really well!! Not sure if it's like this every weekend nights as we've only been here a handful of time. But well done!! The food backed up the line a little and some were sent back (our neighboring table :p but they were not the only ones) because they were undercooked but the team handled it very well!!"
     },
-    review3 = {
+    {
       name: 'Joanna H.',
       stars: 4,
       image: "images/americanreview3.jpg",
@@ -121,7 +121,7 @@ var restaurant4 = {
   image: 'images/american2.jpg',
   info: "Intricately Simple - We are quite Intricate in our preparation, techniques, and craft. Yet, our menu is Simple, just good ol' fashioned grub.",
   reviews: [
-    review1 = {
+    {
      name: 'Truong T.',
      stars: 5,
      image: "images/americanreview4.jpg",
@@ -130,7 +130,7 @@ var restaurant4 = {
      cool: 0,
      content: 'Overall this was a wonderful food truck.  I had a great lunch experience here though it can be a bit pricey.  The total was about 40 bucks which is a bit insane for lunch but sometimes you gotta pay the cost to be a boss.'
    },
-   review2 = {
+   {
      name: 'Stacy S.',
      stars: 5,
      image: "images/americanreview5.jpg",
@@ -139,7 +139,7 @@ var restaurant4 = {
      cool: 0,
      content: 'Super awesome burger food truck!!  I learned of this place because it was stationed right outside of a brewery (Bottle Logic) that we were going to check out. I got their Savory Burger and it was perfection in every bite!  It has bacon relish, Swiss cheese, aioli and 3,000 sauce.'
    },
-   review3 = {
+   {
      name: 'Rob D.',
      stars: 4,
      image: "images/americanreview6.jpg",
@@ -159,7 +159,7 @@ var restaurant5 = {
   image: "images/pizza2.jpg",
   info: 'Authentic Neapolitan Style Pizza made in a 900 degree wood fired oven. Closest to real Italian pizza outside of Naples, Italy.',
   reviews: [
-    review1 = {
+    {
       name: 'Puja L.',
       stars: 4,
       image: "images/pizzareview4.jpg",
@@ -168,7 +168,7 @@ var restaurant5 = {
       cool: 0,
       content: "Mix and match everything!  It's like a dream come true for a person who loves to sample things.  I'm a big fan of variety...and I got it at Pizza Cafe. You can do up to 4 different pizzas on your pie, and order a beer flight of any 4 of the beers they have on tap ($9). I will say that I didn't love their crust, but I was still a pretty happy customer. "
     },
-    review2 = {
+    {
       name: 'Shelly C.',
       stars: 4,
       image: "images/pizzareview5.jpg",
@@ -177,7 +177,7 @@ var restaurant5 = {
       cool: 0,
       content: "At first, we order first class pizza because it's so close to our house- 2 minute of drive, how convenient! AND after we tasted the pizza, we were impressive how good the pizza was! Their crust is the chewy, thicker type, which I love. However, there's also an ultra thin crust option on the menu. Kids like the Sweet Pig (Hawaiian) and plain cheese, my husband and I like the Zesty Italian and Popeye's Favorite. They were generous on toppings, not stingy at all! Our go-to pizza place when I don't feel like cooking!"
     },
-    review3 = {
+    {
       name: 'Denise K.',
       stars: 5,
       image: "images/pizzareview6.jpg",
@@ -197,7 +197,7 @@ var restaurant6 = {
   image: 'images/american3.jpg',
   info: 'Our restaurant serves great burgers, fresh salads, healthy meals and Chicago specialties.  Our catering service specializes in corporate catering, drop off catering, lunch catering, wedding and party catering. Let Aioli create an unforgettable experience for your next event.',
   reviews: [
-    review1 = {
+    {
       name: 'Romuel Dave C.',
       stars: 3,
       image: "images/americanreview7.jpg",
@@ -206,7 +206,7 @@ var restaurant6 = {
       cool: 0,
       content: 'I had really high hopes for this spot.  It is located inside the Main Place food court off Jamboree and Main.  This place has the makings of the perfect burger, but it sort of fell short of great in my book.  Perhaps its my choice on the menu that I made a mistake on... so I wont be too harsh.'
     },
-    review2 = {
+    {
       name: 'Truong T.',
       stars: 4,
       image: "images/americanreview8.jpg",
@@ -215,7 +215,7 @@ var restaurant6 = {
       cool: 0,
       content: "I've been eyeing Aioli for a while now but nobody wanted to go with me for lunch at work to it since my street cred has taken quite a hit from prior bad experiences (curses).  Finally I got somebody to trust me (sucker).  I came through with my coworker to a food court located on Main St.  The place was pretty runned down.  It lacked outer appeal but the interior was actually quite decent."
     },
-    review3 = {
+    {
       name: 'Aaron E.',
       stars: 2,
       image: "images/americanreview9.jpg",
@@ -235,7 +235,7 @@ var restaurant7 = {
   image: "images/pizza3.jpg",
   info: 'MOD Pizza specializes in individual artisan-style pizzas Made On Demand (MOD) in a fast, friendly and stylish environment. All pizzas are cooked by hand in an 800 degree display oven and ready in under 5 minutes with as many toppings as you like for one low price. You can also create your own salads. Northwest beers on tap, wine, sodas and milkshakes. Takeout, awesome music and patio dining.',
   reviews: [
-    review1 = {
+    {
       name: 'Janet R.',
       stars: 5,
       image: "images/pizzareview7.jpg",
@@ -244,7 +244,7 @@ var restaurant7 = {
       cool: 0,
       content: 'This is probably my favorite make your own pizza place. They have a variety of toppings to choose from. I usually like to pack my pizza with a lot of veggies and places like Blaze and Pieology get the crust soggy, but here the crust stays nice and crisp!'
     },
-    review2 = {
+    {
       name: 'Nijole T.',
       stars: 5,
       image: "images/pizzareview8.jpg",
@@ -253,7 +253,7 @@ var restaurant7 = {
       cool: 0,
       content: 'Really falling in love with MOD! More options than any other places where you can create your own pizza, different size pizzas and salads, several tasty and fresh juices instead of soda, desserts and more toppings than any other place offers! Prices and quality are great, this is the perfect place to take kids and picky eaters, you can be healthy or not, everything is fresh and made in house, even the salad dressings which I also love. Service wass very nice, they are a bit slower and your food takes longer than other chains, but the wait is well worth it. Cool and clean location, plenty of seating, worth checking out!'
     },
-    review3 = {
+    {
       name: 'Fernando C.',
       stars: 4,
       image: "images/pizzareview9.jpg",
@@ -273,7 +273,7 @@ var restaurant8 = {
   image: "images/mexican2.jpg",
   info: 'Contemporary Mexican Grill with an edge. Everything from our spicy fire-roasted salsas, blackened seasonings, hand-made tortillas & agua frescas.',
   reviews: [
-    review1 = {
+    {
       name: 'Matt K.',
       stars: 4,
       image: "images/mexicanreview4.jpg",
@@ -282,7 +282,7 @@ var restaurant8 = {
       cool: 0,
       content: "'Excellent Mexican food in the Orchard Hills Plaza.  It's a quick order type of place like a Rubio's.  I think the food tastes fresh and clean.  My carnitas burrito was not heavy at all.  It also came with guacamole on the side which was good and tortilla chips which were also good.  I don't like the salsas at their salsa bar, but the pico de gallo is good.'"
     },
-    review2 = {
+    {
       name: 'Aileen K.',
       stars: 3,
       image: "images/mexicanreview5.jpg",
@@ -291,7 +291,7 @@ var restaurant8 = {
       cool: 0,
       content: "'My friend and I shared the chicken nachos and the chicken burrito. Both came with nice portions of delicious guacamole, which was amazing and fresh. However, the burrito was quite plain and small. Nachos are, well, nachos (delicious and filling but gluttonous). Additionally, the salsas were mediocre.'"
     },
-    review3 = {
+    {
       name: 'Tom',
       stars: 4,
       image: "images/mexicanreview6.jpg",
@@ -311,7 +311,7 @@ var restaurant9 = {
   image: "images/mexican3.jpg",
   info: 'Super Mex offers great-tasting, traditional Mexican food that is prepared to taste homemade. Our signature "Super Mex Burrito" is the perfect example of classic Mexican comfort food prepared with a distinct homemade authenticity. Our extra-grande tortilla chips, legendary salsa, and tender pork carnitas are among the staples here at Super Mex.',
   reviews: [
-    review1 = {
+    {
       name: 'A. P.',
       stars: 2,
       image: "images/mexicanreview7.jpg",
@@ -320,7 +320,7 @@ var restaurant9 = {
       cool: 0,
       content: "'Good food and good service.  There were 5 of us seated at a table with a bench on one side and two chairs in the other.  No one can sit at the end of the table so three of us sat on one side while two sat on the other.  Very uncomfortable when one is left-handed and has all but 1 foot of space to work with.  I honestly don't know what they were thinking by seating 5 adults at a table designed for 4.  It was a busy Saturday night but no excuse for that.  I didn't want to upset the grandparents who were already comfortably seated so we sat through dinner without complaint. '"
     },
-    review2 = {
+    {
       name: 'Jessica E.',
       stars: 3,
       image: "images/mexicanreview8.jpg",
@@ -329,7 +329,7 @@ var restaurant9 = {
       cool: 0,
       content: "'Tonight I ordered 2 meals to go, it was $25 plus tip. My order was 2 cheese enchiladas  crispy asada taco and beans. Well I got about 2 tablespoons of beans if that... the food was cold, I got a soft taco not a Crispy taco. By the time my husband go home it was all cold. we live 5 min away. He even tipped them and when he got home we realized all of this was wrong. I'm super disappointed. I can't believe a place of service would actually serve food this way.  The only reason I gave 2 stars is because the food tasted decent once I reheated it.'"
     },
-    review3 = {
+    {
       name: 'Victoria S.',
       stars: 4,
       image: "images/mexicanreview9.jpg",
@@ -348,6 +348,7 @@ var userRestuarant = {};
 //failH1.appendChild(searchFail);
 var restaurantArray = [ restaurant1, restaurant2, restaurant3, restaurant4, restaurant5, restaurant6, restaurant7, restaurant8, restaurant9 ];
 var search;
+var reviewsForSearchedRestaurant;
 var addReviewButton = document.getElementById('add-review');
 var addRestaurantButton = document.getElementById('add-restaurant');
 var restaurants = document.getElementById('restaurants');
@@ -404,7 +405,6 @@ function sortRestaurants(array,b) {
     }
   };
 
-var reviewsForSearchedRestaurant;
 function searchedRestaurants(array){
   var array = array;
   var name = array.name;
@@ -752,7 +752,6 @@ function addRestaurant(event){
     userRestaurantForm.className="hidden well";
     slRes(userRestuarant);
     addReviewButton.className = "btn btn-default";
-    //goHome(event);
 };
 
 function usefulClick(event){
@@ -764,9 +763,9 @@ function usefulClick(event){
   for (var i = 0; i < restaurantArray.length; i++) {
     if (reviewsForSearchedRestaurant === restaurantArray[i].name){
       var array = restaurantArray[i].reviews;
-      for (var i = 0; i<array.length; i++){
-        if (reviewName === array[i].name){
-          useful(array[i]);
+      for (var z = 0; z < array.length; z++){
+        if (reviewName === array[z].name){
+          useful(array[z]);
         }
       }
     }
@@ -778,7 +777,6 @@ function useful(array){
   var count = array.useful;
   var sum1 = (count + 1);
   array.useful = sum1;
-  //console.log(array.useful);
   resetReviews();
 };
 
@@ -791,9 +789,9 @@ function funnyClick(event){
   for (var i = 0; i < restaurantArray.length; i++) {
     if (reviewsForSearchedRestaurant === restaurantArray[i].name){
       var array = restaurantArray[i].reviews;
-      for (var i = 0; i<array.length; i++){
-        if (reviewName === array[i].name){
-          funny(array[i]);
+      for (var z = 0; z < array.length; z++){
+        if (reviewName === array[z].name){
+          funny(array[z]);
         }
       }
     }
@@ -805,7 +803,6 @@ function funny(array){
   var count = array.funny;
   var sum1 = (count + 1);
   array.funny = sum1;
-  //console.log(array.useful);
   resetReviews();
 };
 
@@ -819,13 +816,13 @@ function coolClick(event){
   for (var i = 0; i < restaurantArray.length; i++) {
     if (reviewsForSearchedRestaurant === restaurantArray[i].name){
       var array = restaurantArray[i].reviews;
-      for (var i = 0; i<array.length; i++){
-        if (reviewName === array[i].name){
-          cool(array[i]);
+      for (var z = 0; z < array.length; z++){
+        if (reviewName === array[z].name){
+          cool(array[z]);
         }
       }
     }
-  }
+  };
 };
 
 function cool(array){
@@ -834,7 +831,6 @@ function cool(array){
   var count = array.cool;
   var sum1 = (count + 1);
   array.cool = sum1;
-  //console.log(array.useful);
   resetReviews();
 };
 
